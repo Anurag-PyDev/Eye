a4ec8895fa9c56b42bd55431b3c26c69aeb9b055
Anurag-PyDev <as1235374@gmail.com>
2024-02-05 20:15:27 +0530

    added the bg and eyes

--- main.py
+++ main.py
@@ -1,1 +1,58 @@
-import pygame
+import pygame,sys
+from pygame import *
+
+
+def mainloop():
+    while True:
+
+        for eve in event.get():
+            if eve.type == QUIT:
+                quit()
+                sys.exit()
+
+        blits()
+        
+        display.flip()
+
+
+class Eye:
+    def __init__(self):
+        self.x= 0
+        self.y = 0
+        self.rad = 0
+        self.color = EYECOLOR
+    
+    def blit(self,surface,x,y,rad):
+        self.x= x
+        self.y = y
+        self.rad = rad
+        draw.circle(surface,self.color,(self.x,self.y),self.rad)
+
+
+
+
+def blits():
+    draw.rect(root,BGCOLOR,(0,0,root.get_width(),root.get_height()))
+    e1.blit(root,0.3*root.get_width(),0.45*root.get_height(),0.2*root.get_height())
+    e2.blit(root,0.7*root.get_width(),0.45*root.get_height(),0.2*root.get_height())
+
+
+
+if __name__ == "__main__":
+
+
+    pygame.init()
+    root = display.set_mode((800,600),pygame.RESIZABLE)
+    BGCOLOR = (255,190,175)
+    EYECOLOR = (255,255,255)
+    e1 = Eye()
+    e2 = Eye()
+    
+    
+    
+    
+    
+    print(root.get_width(),root.get_height())
+    mainloop()
+    
+            
